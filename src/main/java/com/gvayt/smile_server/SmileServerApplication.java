ab46336f3587ae4e3311a1f8fcace4ac11c02469
Georgee Vaitukov <gvayt77@gmail.com>
2026-06-12 20:02:06 +0400

    Initial commit

---
+++ src/main/java/com/gvayt/smile_server/SmileServerApplication.java
@@ -0,0 +1,13 @@
+package com.gvayt.smile_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmileServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmileServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gvayt/smile_server/SmileServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gvayt.smile_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmileServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
